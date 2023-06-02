b921534a5c531ef35ae40ea44f6a3632822d414e
Ajayreddy17 <96877307+Ajayreddy17@users.noreply.github.com>
2023-06-02 21:10:48 +0530

    Create runlengthencoding.cpp

---
+++ Utility/runlengthencoding.cpp
@@ -0,0 +1,19 @@
+// coded by Nyaan (https://codeforces.com/profile/Nyaan)
+template <typename C>
+vector<pair<typename C::value_type, int>> RunLengthEncoding(C& S) {
+    using T = typename C::value_type;
+    if (S.empty()) return {};
+    vector<pair<T, int>> ret;
+    T c = S[0];
+    int n = 1;
+    for (int i = 1; i < (int)S.size(); i++) {
+        if (S[i] == c)
+            n++;
+        else {
+            ret.emplace_back(c, n);
+            c = S[i], n = 1;
+        }
+    }
+    ret.emplace_back(c, n);
+    return ret;
+}
