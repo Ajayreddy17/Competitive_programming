4a9633903aaf3ce1e2c5f0b87676a5274f237066
Ajayreddy17 <96877307+Ajayreddy17@users.noreply.github.com>
2023-06-13 10:46:58 +0530

    Create Monoid_MinMax.cpp

---
+++ New/maspy/Ds/Segment_tree/Monoids/Monoid_MinMax.cpp
@@ -0,0 +1,12 @@
+// P.first -> Minimum and P.second -> Maximum
+template <class X>
+struct Monoid_MinMax {
+    using P = pair<X, X>;
+    using value_type = P;
+    static constexpr P op(const P x, const P y) noexcept {
+        return {min(x.fi, y.fi), max(x.se, y.se)};
+    }
+    static constexpr P from_element(const X x) { return {x, x}; }
+    static constexpr P unit() { return {inf<X>, -inf<X>}; }
+    static constexpr bool commute = true;
+};
