4f8f37e2b5e1e5631d507a716c89c8b4bca7d022
AbeedShaikM <mullaabeed2004@gmail.com>
2023-07-03 18:20:10 +0530

    Create ActedMonoid_Max_Max.cpp

---
+++ New/maspy/Ds/Segment_tree/ActedMonoid/ActedMonoid_Max_Max.cpp
@@ -0,0 +1,13 @@
+//requires Monoid_Max
+
+template<typename E>
+struct ActedMonoid_Max_Max {
+    using Monoid_X = Monoid_Max<E>;
+    using Monoid_A = Monoid_Max<E>;
+    using X = typename Monoid_X::value_type;
+    using A = typename Monoid_A::value_type;
+
+    static constexpr X act(const X &x, const A &a, const long long &size) {
+        return max(x, a);
+    }
+};
