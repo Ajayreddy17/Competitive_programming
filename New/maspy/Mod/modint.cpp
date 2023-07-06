6e35a9591d5e4432bf518ee323591df8f12e870b
Ajayreddy17 <96877307+Ajayreddy17@users.noreply.github.com>
2023-07-06 14:51:33 +0530

    Create modint.cpp

---
+++ New/maspy/Mod/modint.cpp
@@ -0,0 +1,75 @@
+template <int mod>
+struct modint {
+    static_assert(mod < (1 << 30));
+    int val;
+    constexpr modint(const long long val = 0) noexcept
+            : val(val >= 0 ? val % mod : (mod - (-val) % mod) % mod) {}
+    bool operator<(const modint &other) const {
+        return val < other.val;
+    } // To use std::map
+    modint &operator+=(const modint &p) {
+        if ((val += p.val) >= mod) val -= mod;
+        return *this;
+    }
+    modint &operator-=(const modint &p) {
+        if ((val += mod - p.val) >= mod) val -= mod;
+        return *this;
+    }
+    modint &operator*=(const modint &p) {
+        val = (int)(1LL * val * p.val % mod);
+        return *this;
+    }
+    modint &operator/=(const modint &p) {
+        *this *= p.inverse();
+        return *this;
+    }
+    modint operator-() const { return modint(-val); }
+    modint operator+(const modint &p) const { return modint(*this) += p; }
+    modint operator-(const modint &p) const { return modint(*this) -= p; }
+    modint operator*(const modint &p) const { return modint(*this) *= p; }
+    modint operator/(const modint &p) const { return modint(*this) /= p; }
+    bool operator==(const modint &p) const { return val == p.val; }
+    bool operator!=(const modint &p) const { return val != p.val; }
+    modint inverse() const {
+        int a = val, b = mod, u = 1, v = 0, t;
+        while (b > 0) {
+            t = a / b;
+            swap(a -= t * b, b), swap(u -= t * v, v);
+        }
+        return modint(u);
+    }
+    modint pow(long long n) const {
+        assert(n >= 0);
+        modint ret(1), mul(val);
+        while (n > 0) {
+            if (n & 1) ret *= mul;
+            mul *= mul;
+            n >>= 1;
+        }
+        return ret;
+    }
+
+    void write() { write(val); }
+    void read() {
+        read(val);
+        val = (val >= 0 ? val % mod : (mod - (-val) % mod) % mod);
+    }
+
+    static constexpr int get_mod() { return mod; }
+    // (n, r), r は 1 の 2^n 乗根
+    static constexpr pair<int, int> ntt_info() {
+        if (mod == 167772161) return {25, 17};
+        if (mod == 469762049) return {26, 30};
+        if (mod == 754974721) return {24, 362};
+        if (mod == 880803841) return {23, 211};
+        if (mod == 998244353) return {23, 31};
+        if (mod == 1045430273) return {20, 363};
+        if (mod == 1051721729) return {20, 330};
+        if (mod == 1053818881) return {20, 2789};
+        return {-1, -1};
+    }
+    static constexpr bool can_ntt() { return ntt_info().first != -1; }
+};
+
+using modint107 = modint<1000000007>;
+using modint998 = modint<998244353>;
