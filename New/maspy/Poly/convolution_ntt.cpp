7413c467ef514664bfc94a17267b6cbcab903ead
Ajayreddy17 <96877307+Ajayreddy17@users.noreply.github.com>
2023-07-06 15:28:15 +0530

    Create convolution_ntt.cpp

---
+++ New/maspy/Poly/convolution_ntt.cpp
@@ -0,0 +1,167 @@
+template <class mint>
+vector<mint> convolution_ntt(vector<mint> a, vector<mint> b) {
+    if (a.empty() || b.empty()) return {};
+    int n = int(a.size()), m = int(b.size());
+    int sz = 1;
+    while (sz < n + m - 1) sz *= 2;
+
+    // sz = 2^k のときの高速化。分割統治的なやつで損しまくるので。
+    if ((n + m - 3) <= sz / 2) {
+        auto a_last = a.back(), b_last = b.back();
+        a.pop_back(), b.pop_back();
+        auto c = convolution(a, b);
+        c.resize(n + m - 1);
+        c[n + m - 2] = a_last * b_last;
+        for(int i = 0; i < (int) a.size(); i++) c[i + (int) b.size()] += a[i] * b_last;
+        for(int i = 0; i < (int) b.size(); i++) c[i + (int) a.size()] += b[i] * a_last;
+        return c;
+    }
+
+    a.resize(sz), b.resize(sz);
+    bool same = a == b;
+    ntt(a, 0);
+    if (same) {
+        b = a;
+    } else {
+        ntt(b, 0);
+    }
+    for(int i = 0; i < sz; i++) a[i] *= b[i];
+    ntt(a, 1);
+    a.resize(n + m - 1);
+    return a;
+}
+
+template <typename mint>
+vector<mint> convolution_garner(const vector<mint>& a, const vector<mint>& b) {
+    int n = (int) a.size(), m = (int) b.size();
+    if (!n || !m) return {};
+    static const long long nttprimes[] = {754974721, 167772161, 469762049};
+    using mint0 = modint<754974721>;
+    using mint1 = modint<167772161>;
+    using mint2 = modint<469762049>;
+    vector<mint0> a0(n), b0(m);
+    vector<mint1> a1(n), b1(m);
+    vector<mint2> a2(n), b2(m);
+    for(int i = 0; i < n; i++) a0[i] = a[i].val, a1[i] = a[i].val, a2[i] = a[i].val;
+    for(int i = 0; i < m; i++) b0[i] = b[i].val, b1[i] = b[i].val, b2[i] = b[i].val;
+    auto c0 = convolution_ntt<mint0>(a0, b0);
+    auto c1 = convolution_ntt<mint1>(a1, b1);
+    auto c2 = convolution_ntt<mint2>(a2, b2);
+    static const long long m01 = 1LL * nttprimes[0] * nttprimes[1];
+    static const long long m0_inv_m1 = mint1(nttprimes[0]).inverse().val;
+    static const long long m01_inv_m2 = mint2(m01).inverse().val;
+    const int mod = mint::get_mod();
+    auto garner = [&](mint0 x0, mint1 x1, mint2 x2) -> mint {
+        int r0 = x0.val, r1 = x1.val, r2 = x2.val;
+        int v1 = (m0_inv_m1 * (r1 + nttprimes[1] - r0)) % nttprimes[1];
+        auto v2 = (mint2(r2) - r0 - mint2(nttprimes[0]) * v1) * mint2(m01_inv_m2);
+        return mint(r0 + 1LL * nttprimes[0] * v1 + m01 % mod * v2.val);
+    };
+    vector<mint> c((int) c0.size());
+    for(int i = 0; i < (int) c.size(); i++) c[i] = garner(c0[i], c1[i], c2[i]);
+    return c;
+}
+
+template <typename R>
+vector<double> convolution_fft(const vector<R>& a, const vector<R>& b) {
+    using C = CFFT::C;
+    int need = (int)a.size() + (int)b.size() - 1;
+    int nbase = 1;
+    while ((1 << nbase) < need) nbase++;
+    CFFT::ensure_base(nbase);
+    int sz = 1 << nbase;
+    vector<C> fa(sz);
+    for (int i = 0; i < sz; i++) {
+        int x = (i < (int)a.size() ? a[i] : 0);
+        int y = (i < (int)b.size() ? b[i] : 0);
+        fa[i] = C(x, y);
+    }
+    CFFT::fft(fa, sz);
+    C r(0, -0.25 / (sz >> 1)), s(0, 1), t(0.5, 0);
+    for (int i = 0; i <= (sz >> 1); i++) {
+        int j = (sz - i) & (sz - 1);
+        C z = (fa[j] * fa[j] - (fa[i] * fa[i]).conj()) * r;
+        fa[j] = (fa[i] * fa[i] - (fa[j] * fa[j]).conj()) * r;
+        fa[i] = z;
+    }
+    for (int i = 0; i < (sz >> 1); i++) {
+        C A0 = (fa[i] + fa[i + (sz >> 1)]) * t;
+        C A1 = (fa[i] - fa[i + (sz >> 1)]) * t * CFFT::rts[(sz >> 1) + i];
+        fa[i] = A0 + A1 * s;
+    }
+    CFFT::fft(fa, sz >> 1);
+    vector<double> ret(need);
+    for (int i = 0; i < need; i++) {
+        ret[i] = (i & 1 ? fa[i >> 1].y : fa[i >> 1].x);
+    }
+    return ret;
+}
+
+vector<long long> convolution(const vector<long long>& a, const vector<long long>& b) {
+    int n = (int) a.size(), m = (int) b.size();
+    if (!n || !m) return {};
+    if (min(n, m) <= 60) return convolution_naive(a, b);
+    long long abs_sum_a = 0, abs_sum_b = 0;
+    long long LIM = 1e15;
+    for(int i = 0; i < n; i++) abs_sum_a = min(LIM, abs_sum_a + abs(a[i]));
+    for(int i = 0; i < m; i++) abs_sum_b = min(LIM, abs_sum_b + abs(b[i]));
+    if (__int128(abs_sum_a) * abs_sum_b < 1e15) {
+        vector<double> c = convolution_fft<long long>(a, b);
+        vector<long long> res((int) c.size());
+        for(int i = 0; i < (int) c.size(); i++) res[i] = (long long)(floor(c[i] + .5));
+        return res;
+    }
+
+    static constexpr unsigned long long MOD1 = 754974721; // 2^24
+    static constexpr unsigned long long MOD2 = 167772161; // 2^25
+    static constexpr unsigned long long MOD3 = 469762049; // 2^26
+    static constexpr unsigned long long M2M3 = MOD2 * MOD3;
+    static constexpr unsigned long long M1M3 = MOD1 * MOD3;
+    static constexpr unsigned long long M1M2 = MOD1 * MOD2;
+    static constexpr unsigned long long M1M2M3 = MOD1 * MOD2 * MOD3;
+
+    static const unsigned long long i1 = mod_inv(MOD2 * MOD3, MOD1);
+    static const unsigned long long i2 = mod_inv(MOD1 * MOD3, MOD2);
+    static const unsigned long long i3 = mod_inv(MOD1 * MOD2, MOD3);
+
+    using mint1 = modint<MOD1>;
+    using mint2 = modint<MOD2>;
+    using mint3 = modint<MOD3>;
+
+    vector<mint1> a1(n), b1(m);
+    vector<mint2> a2(n), b2(m);
+    vector<mint3> a3(n), b3(m);
+    for(int i = 0; i < n; i++) a1[i] = a[i], a2[i] = a[i], a3[i] = a[i];
+    for(int i = 0; i < m; i++) b1[i] = b[i], b2[i] = b[i], b3[i] = b[i];
+
+    auto c1 = convolution_ntt<mint1>(a1, b1);
+    auto c2 = convolution_ntt<mint2>(a2, b2);
+    auto c3 = convolution_ntt<mint3>(a3, b3);
+
+    vector<long long> c(n + m - 1);
+    for(int i = 0; i < n + m - 1; i++) {
+        unsigned long long x = 0;
+        x += (c1[i].val * i1) % MOD1 * M2M3;
+        x += (c2[i].val * i2) % MOD2 * M1M3;
+        x += (c3[i].val * i3) % MOD3 * M1M2;
+        long long diff = c1[i].val - ((long long)(x) % (long long)(MOD1));
+        if (diff < 0) diff += MOD1;
+        static constexpr unsigned long long offset[5]
+                = {0, 0, M1M2M3, 2 * M1M2M3, 3 * M1M2M3};
+        x -= offset[diff % 5];
+        c[i] = x;
+    }
+    return c;
+}
+
+template <typename mint>
+vector<mint> convolution(const vector<mint>& a, const vector<mint>& b) {
+    int n = (int) a.size(), m = (int) b.size();
+    if (!n || !m) return {};
+    if (mint::can_ntt()) {
+        if (min(n, m) <= 50) return convolution_naive(a, b);
+        return convolution_ntt(a, b);
+    }
+    if (min(n, m) <= 200) return convolution_naive(a, b);
+    return convolution_garner(a, b);
+}
