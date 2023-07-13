1243bac94d56d0936cc0c939fd5607dcf297336e
Ajayreddy17 <96877307+Ajayreddy17@users.noreply.github.com>
2023-07-13 16:20:04 +0530

    Update ActedMonoid_MaxIdx_Add.cpp

--- New/maspy/Ds/Segment_tree/ActedMonoid/ActedMonoid_MaxIdx_Add.cpp
+++ New/maspy/Ds/Segment_tree/ActedMonoid/ActedMonoid_MaxIdx_Add.cpp
@@ -5,7 +5,7 @@ struct ActedMonoid_MaxIdx_Add {
     using X = typename Monoid_X::value_type;
     using A = typename Monoid_A::value_type;
     static constexpr X act(const X &x, const A &a, const long long &size) {
-        if (x.fi == -inf<E>) return x;
-        return {x.fi + a, x.se};
+        if (x.first == -inf<E>) return x;
+        return {x.first + a, x.second};
     }
 };
